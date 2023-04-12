6176c571ac6d9461c16500a2169762dd41beab77
Sahith <87948601+thadimudupulasahith@users.noreply.github.com>
2023-04-12 13:28:46 +0530

    Update project.py

--- project.py
+++ project.py
@@ -5,11 +5,17 @@ from snscrape.modules import twitter
 import pymongo
 
 # Set up MongoDB client and database
+# Connect to the MongoDB Atlas cluster using the connection string
 client = pymongo.MongoClient('mongodb+srv://thadimudupulasahith:sahith@cluster0.cy57gio.mongodb.net/?retryWrites=true&w=majority')
+# Access the 'twitter_data' database
 db = client['twitter_data']
+# Access the 'tweets' collection
 collection = db['tweets']
 
 # Define function to scrape Twitter data
+# This function takes a search query, start date, end date, and tweet count as inputs
+# It uses the snscrape library to scrape tweets that match the search query and fall within the specified date range
+# The function returns a pandas DataFrame containing the scraped tweets
 def scrape_twitter_data(search_query, start_date, end_date, tweet_count):
     tweets = []
     for tweet in twitter.TwitterSearchScraper(f'{search_query} since:{start_date} until:{end_date}').get_items():
@@ -30,6 +36,8 @@ def scrape_twitter_data(search_query, start_date, end_date, tweet_count):
     return pd.DataFrame(tweets)
 
 # Define function to upload data to MongoDB
+# This function takes a pandas DataFrame and a search query as inputs
+# It uploads the DataFrame to the MongoDB Atlas cluster in the 'twitter_data' database and 'tweets' collection
 def upload_to_mongodb(data, search_query):
     collection.insert_one({
         'Scraped Word': search_query,
@@ -38,6 +46,7 @@ def upload_to_mongodb(data, search_query):
     })
 
 # Set up Streamlit app
+# This line of code initializes a Streamlit app with the title 'Twitter Data Scraper'
 st.title('Twitter Data Scraper')
 
 # Get user input for search query, start date, end date, and tweet count
@@ -47,30 +56,41 @@ end_date = st.date_input('Select an end date:')
 tweet_count = st.number_input('Enter the maximum number of tweets to scrape:', min_value=1, max_value=1000, value=100)
 
 # Convert date inputs to string format
+# These lines of code convert the start date and end date inputs to string format
 start_date_str = start_date.strftime('%Y-%m-%d')
 end_date_str = end_date.strftime('%Y-%m-%d')
 
 data = pd.DataFrame()
 
 # Scrape Twitter data and display in table
+# This line of code creates a button that, when clicked, initiates the Twitter data scraping process
 if st.button('Scrape Data'):
+    # Display message that the data is being scraped
     st.write(f'Scraping Twitter data for "{search_query}" from {start_date_str} to {end_date_str}...')
+    # Scrape the data using the given parameters
     data = scrape_twitter_data(search_query, start_date_str, end_date_str, tweet_count)
+    # Display the number of tweets that were scraped
     st.write(f'{len(data)} tweets scraped:')
+    # Display the scraped data in a table
     st.dataframe(data)
     # Upload data to MongoDB
     upload_to_mongodb(data, search_query)
+    # Display message that the data has been uploaded
     st.write('Data uploaded to MongoDB.')
 
     # Add buttons to download data in CSV and JSON formats
     try:
+         # Convert the data to CSV format and create a download button for it
         csv = data.to_csv(index=False)
         st.download_button('Download CSV', data=csv, file_name='twitter_data.csv', mime='text/csv')
+        # Display an error message if the CSV download button cannot be created
     except Exception as e:
         st.write('Error downloading CSV file:', e)
 
     try:
+         # Convert the data to JSON format and create a download button for it
         json = data.to_json(orient='records')
         st.download_button('Download JSON', data=json, file_name='twitter_data.json', mime='application/json')
+         # Display an error message if the JSON download button cannot be created
     except Exception as e:
         st.write('Error downloading JSON file:', e)
